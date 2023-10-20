f40b7b79c0485f6d6549ca32bc2122d712f7bc4b
Paulo Paiva <paulojr.eco@gmail.com>
2023-10-19 23:00:14 -0300

    feat: ensure product creation is valid

--- domain/model/product.go
+++ domain/model/product.go
@@ -3,6 +3,7 @@ package model
 import (
 	"time"
 
+	"github.com/asaskevich/govalidator"
 	uuid "github.com/satori/go.uuid"
 )
 
@@ -11,6 +12,15 @@ type Product struct {
 	Name        string  `json:"name" gorm:"type:varchar(50);not null" valid:"notnull"`
 	Description string  `json:"description" gorm:"type:varchar(255);not null" valid:"notnull"`
 	Price       float64 `json:"price" gorm:"type:float" valid:"notnull"`
+}
+
+func (product *Product) isValid() error {
+	_, err := govalidator.ValidateStruct(product)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func NewProduct(name string, description string, price float64) (*Product, error) {
@@ -22,6 +32,11 @@ func NewProduct(name string, description string, price float64) (*Product, error
 
 	product.ID = uuid.NewV4().String()
 	product.CreatedAt = time.Now()
+
+	err := product.isValid()
+	if err != nil {
+		return nil, err
+	}
 
 	return &product, nil
 }

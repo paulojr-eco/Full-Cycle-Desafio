854a7cdfc5e630ab83947cb14b241152d4ef5621
Paulo Paiva <paulojr.eco@gmail.com>
2023-10-20 00:37:26 -0300

    test: ensure db returns an error if the connection url is wrong

---
+++ infra/db/db_test.go
@@ -0,0 +1,21 @@
+package db_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/paulojr-eco/full-cycle-desafio/infra/db"
+	"github.com/stretchr/testify/require"
+)
+
+/* Should return an error if the connection url is wrong */
+func TestDb_NewWrongConection(t *testing.T) {
+	dbType := os.Getenv("dbType")
+	t.Setenv("dbType", "wrong_database_connection")
+	database, err := db.ConnectDB()
+
+	require.NotNil(t, err)
+	require.Nil(t, database)
+
+	t.Cleanup(func() { os.Setenv("dbType", dbType) })
+}

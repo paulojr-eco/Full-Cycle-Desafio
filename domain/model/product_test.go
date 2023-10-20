b65d6ec367713dee4ae96c219209f7ecacbcec0e
Paulo Paiva <paulojr.eco@gmail.com>
2023-10-19 23:00:22 -0300

    test: ensure product creation is valid

---
+++ domain/model/product_test.go
@@ -0,0 +1,34 @@
+package model_test
+
+import (
+	"testing"
+
+	"github.com/paulojr-eco/full-cycle-desafio/domain/model"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+/* Should throw an error on validation fail */
+func TestModel_NewWrongProduct_MissingParams(t *testing.T) {
+	price := 15.99
+
+	_, err := model.NewProduct("", "", price)
+	require.NotNil(t, err)
+}
+
+/* Should return a product on success */
+func TestModel_NewProduct(t *testing.T) {
+	name := "Product 01"
+	description := "Description from product 01"
+	price := 15.99
+
+	product, err := model.NewProduct(name, description, price)
+
+	require.Nil(t, err)
+	require.NotEmpty(t, uuid.FromStringOrNil(product.ID))
+	require.Equal(t, product.Name, name)
+	require.Equal(t, product.Description, description)
+	require.Equal(t, product.Price, price)
+	require.NotNil(t, product.CreatedAt)
+	require.True(t, product.UpdatedAt.IsZero())
+}

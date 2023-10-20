7fde81edbf40a74cdcba07175deac26ec6bf00d2
Paulo Paiva <paulojr.eco@gmail.com>
2023-10-20 04:44:38 -0300

    refactor: remove commentaries

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -9,27 +6,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "full-cycle-desafio",
-	Short: "This software was created in the event Imersão Full Cycle and it makes te management of products that can be created or listed",
+	Short: "This software was created in the event Imersão Full Cycle and it makes the management of products that can be created and listed",
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	database, _ := db.ConnectDB()
 	grpc.StartGrpcServer(database, 50051)
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.full-cycle-desafio.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolP("toggle", "t", false, "Exec 'go run main.go' to start the server")
 }
